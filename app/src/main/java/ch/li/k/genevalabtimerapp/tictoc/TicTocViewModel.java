0a1d1702979a64c50e0fb5dd8c9a3164d265839b
kli <din4yzev>
2019-05-15 15:04:10 +0200

    Start wiring up view model.

--- app/src/main/java/ch/li/k/genevalabtimerapp/tictoc/TicTocViewModel.java
+++ app/src/main/java/ch/li/k/genevalabtimerapp/tictoc/TicTocViewModel.java
@@ -6,7 +6,6 @@ import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 import android.view.View;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -15,18 +14,18 @@ import java.util.concurrent.Executors;
 public class TicTocViewModel extends AndroidViewModel {
 
     private ExecutorService executorService;
-    private MutableLiveData<TicTocModel> modelMutableLiveData;
-    private MutableLiveData<LocalDateTime> timestampMutableLiveData;
+    private MutableLiveData<TicTocModel> mutableTicTocModel;
+    private MutableLiveData<LocalDateTime> mutableTimestamp;
     private MutableLiveData<List<TicTocModel>> mutableTicTocModelList;
 
     public TicTocViewModel(@NonNull Application application) {
         super(application);
 
-        this.modelMutableLiveData = new MutableLiveData<>();
+        this.mutableTicTocModel = new MutableLiveData<>();
         this.mutableTicTocModelList = new MutableLiveData<>();
 
-        this.timestampMutableLiveData = new MutableLiveData<>();
-        this.timestampMutableLiveData.setValue(LocalDateTime.now());
+        this.mutableTimestamp = new MutableLiveData<>();
+        this.mutableTimestamp.setValue(LocalDateTime.now());
 
         this.executorService = Executors.newSingleThreadExecutor();
     }
@@ -40,35 +39,29 @@ public class TicTocViewModel extends AndroidViewModel {
     }
 
     private void newEntry() {
-//        List<TicTocModel> modelList = new ArrayList<>();
-//        TicTocModel model = this.modelMutableLiveData.getValue();
-        TicTocModel model = new TicTocModel(new String[]{"a", "b"});
-        modelMutableLiveData.setValue(model);
+        List<TicTocModel> ticTcModelList = mutableTicTocModelList.getValue();
+        ticTcModelList.add(new TicTocModel(new String[]{"a", "b"}));
+        mutableTicTocModelList.setValue(ticTcModelList);
     }
 
     public void writeEntry(View v) {
         newEntry();
-        try {
-            this.modelMutableLiveData.getValue().writeCsv();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            this.modelMutableLiveData.getValue().writeCsv();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public MutableLiveData<LocalDateTime> getMutableTimestamp() {
+        return mutableTimestamp;
     }
 
     public void newTimestamp(View v) {
-        this.timestampMutableLiveData.setValue(LocalDateTime.now());
-        System.out.println(this.timestampMutableLiveData.getValue().toString());
+        this.mutableTimestamp.setValue(LocalDateTime.now());
     }
 
-    public MutableLiveData<LocalDateTime> getTimestampMutableLiveData() {
-        return timestampMutableLiveData;
-    }
-
-    public void setTimestampMutableLiveData(MutableLiveData<LocalDateTime> timestampMutableLiveData) {
-        this.timestampMutableLiveData = timestampMutableLiveData;
-    }
-
-    //    public TicTocViewModel(@NonNull Application application) {
+//    public TicTocViewModel(@NonNull Application application) {
 //        super(application);
 //    }
 //
